4f722e56e6273d3b48d14d3464e33440c63c3ef5
nikita3331 <40238256+nikita3331@users.noreply.github.com>
2020-09-24 17:46:53 +0200

    added new files

--- program/testing.py
+++ program/testing.py
@@ -28,23 +28,20 @@ def createPoints():
 
 
 
-# def createDepth(numberOfPointsInLine):
-#     depthArray=[]
-#     for i in range(0,1000):
-#         row=[]
-#         angles=np.linspace(0,4*math.pi,numberOfPointsInLine)
-#         for i in angles:
-#             row.append(10*np.sin(i))
-#         depthArray.append(row)
-#     return depthArray
 def createDepth(numberOfPointsInLine):
     depthArray=[]
-    angles=np.linspace(0,6*math.pi,1000)
-    for angle in angles:
-        distance=math.sin(angle)
-        row=[distance]*numberOfPointsInLine
+    for i in range(0,100):
+        row=np.linspace(0,1,numberOfPointsInLine)
         depthArray.append(row)
     return depthArray
+# def createDepth(numberOfPointsInLine):
+#     depthArray=[]
+#     angles=np.linspace(0,6*math.pi,1000)
+#     for angle in angles:
+#         distance=math.sin(angle)
+#         row=[distance]*numberOfPointsInLine
+#         depthArray.append(row)
+#     return depthArray
 def showDiagram(x,y,z,color):
     fig = plt.figure()
     ax = Axes3D(fig)
@@ -56,44 +53,43 @@ def showDiagram(x,y,z,color):
 def flatCoords(x,y,z):
     fullArr=[]
     colors=[]
-    colorspace=np.linspace(0,1,np.shape(x)[0])
-    i=0
-    for (rowx, rowy, rowz) in zip(x, y, z):
-        rowColor=[colorspace[i],0,0]
-        for (it_x, it_y, it_z) in zip(rowx, rowy, rowz):  
+    red=np.linspace(0,1,np.shape(x)[0])
+    green=np.linspace(0,1,np.shape(x)[1])
+
+    for (rowx, rowy, rowz,color_red) in zip(x, y, z,red):
+        for (it_x, it_y, it_z,color_green) in zip(rowx, rowy, rowz,green):  
             fullArr.append([it_x, it_y, it_z] )
-            colors.append(rowColor)
-        i=i+1
+            colors.append([color_red,color_green,0])
     return fullArr,colors
 def createDelaunayMesh(flat,color):
-    # tri = Delaunay(flat)
-    # faces=[]
-    # for i in tri.simplices:
-    #     faces.append([i[0],i[1],i[2]])
-    # probnymesh = trimesh.Trimesh(vertices=flat,faces=faces,vertex_colors=color)
-    # probnymesh.export('testDelaunayMesh.ply')
-    # pcd_load = o3d.io.read_triangle_mesh("testDelaunayMesh.ply")
-    # o3d.visualization.draw_geometries([pcd_load],window_name='delaunay')
+    tri = Delaunay(flat)
+    faces=[]
+    for i in tri.simplices:
+        faces.append([i[0],i[1],i[2]])
+    probnymesh = trimesh.Trimesh(vertices=flat,faces=faces,vertex_colors=color)
+    probnymesh.export('testDelaunayMesh.ply')
+    pcd_load = o3d.io.read_triangle_mesh("testDelaunayMesh.ply")
+    o3d.visualization.draw_geometries([pcd_load],window_name='delaunay')
 
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(flat)
     pcd.colors =  o3d.utility.Vector3dVector(color)
-    # o3d.visualization.draw_geometries([pcd],window_name='point cloud')
+    o3d.visualization.draw_geometries([pcd],window_name='point cloud')
 
-    pcd.estimate_normals()
-    distances = pcd.compute_nearest_neighbor_distance()
-    avg_dist = np.mean(distances)
-    radius =15*avg_dist
-    bpa_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd,o3d.utility.DoubleVector([radius, radius * 2] ))
-    tri_mesh = trimesh.Trimesh(np.asarray(bpa_mesh.vertices), np.asarray(bpa_mesh.triangles),vertex_normals=np.asarray(bpa_mesh.vertex_normals),vertex_colors=pcd.colors)
-    tri_mesh.export('testbpamesh.ply')
-    pcd_load = o3d.io.read_triangle_mesh("testbpamesh.ply")
-    o3d.visualization.draw_geometries([pcd_load],window_name='bpa cloud')
+    # pcd.estimate_normals()
+    # distances = pcd.compute_nearest_neighbor_distance()
+    # avg_dist = np.mean(distances)
+    # radius =3*avg_dist
+    # bpa_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd,o3d.utility.DoubleVector([radius, radius * 2] ))
+    # tri_mesh = trimesh.Trimesh(np.asarray(bpa_mesh.vertices), np.asarray(bpa_mesh.triangles),vertex_normals=np.asarray(bpa_mesh.vertex_normals),vertex_colors=pcd.colors)
+    # tri_mesh.export('testbpamesh.ply')
+    # pcd_load = o3d.io.read_triangle_mesh("testbpamesh.ply")
+    # o3d.visualization.draw_geometries([pcd_load],window_name='bpa cloud')
 
 
     
 
 x,y,z=createPoints()
 flat,color=flatCoords(x,y,z)
-showDiagram(x,y,z,color)
+# showDiagram(x,y,z,color)
 createDelaunayMesh(flat,color)
