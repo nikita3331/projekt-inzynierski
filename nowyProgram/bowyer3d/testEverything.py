5fc6858c3f2679beb99a9c73a7c26b91046e3f74
mykyta brazhynskyy <mykyta.brazhynskyy@gmail.com>
2020-10-08 00:20:30 +0200

    Update testEverything.py

--- nowyProgram/bowyer3d/testEverything.py
+++ nowyProgram/bowyer3d/testEverything.py
@@ -5,22 +5,51 @@ import matplotlib.pyplot as plt
 import math
 from random import random
 from Point3D import Point
+from timeit import default_timer as timer
+import multiprocess 
 
-xsys=[(3,3),(4,3),(5,2),(4,1),(3,1),(2,2)] #hex
-# xsys=[(1,1),(0,2),(2,2),(1,3)] #square
-#xsys=[(1,1),(0,2),(2,3),(1,3),(2,1)] #five
 
-xsyszs=[]
-for itera in range(0,40):
-    point=Point(random()*100,random()*100,random()*100)
-    xsyszs.append(point)
-# xsyszs=[Point(0,0,0),Point(20,0,0),Point(0,0,30),Point(0,20,0)]
+def count(items):
+    xsyszs=[]
+    for itera in range(0,items):
+        point=Point(random()*100,random()*100,random()*100)
+        xsyszs.append(point)
+    # xsyszs=[Point(0,0,0),Point(20,0,0),Point(0,0,30),Point(0,20,0)]
+    start = timer()
+    comp=Delaunay(xsyszs)
+    transformed,normalPoints=comp.computeVertices()
+    # comp.plotSelf()
+    end = timer()
+    times.append(end-start)
+    xaxis.append(items)
+    
+    print('koniec ',items)
+    return (times,xaxis)
 
-comp=Delaunay(xsyszs)
-transformed,normalPoints=comp.computeVertices()
-comp.plotSelf()
-# xs=[point[0] for point in xsys]
-# ys=[point[1] for point in xsys]
 
-# plt.scatter(xs,ys)
-# plt.show()
+
+def initializer():
+    global times
+    times = []
+    global xaxis
+    xaxis = []
+    
+if __name__ == '__main__':
+    
+
+    pool = multiprocess.Pool(1,initializer,())
+    time=pool.map(count, range(4,25))
+    pool.close()
+    time.sort(key=lambda x:len(x),reverse=True)
+    myTimes=time[0][0]
+    myInd=time[0][1]
+
+    filed=[]
+    for tim,ind in zip(myTimes,myInd):
+        filed.append((tim,ind))
+    filed.sort(key=lambda x:x[1],reverse=False)
+    
+    
+    plt.plot(filed[:][1],filed[:][0])
+    plt.show()
+    print('koniec')
