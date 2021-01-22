e8e6a9e3d60698f5a656eb0e9012ec9aec452c77
Mykyta Brazhynskyy <nikita3331@gmail.com>
2021-01-22 23:26:13 +0100

    add

--- nowyProgram/bowyer/testEverything.py
+++ nowyProgram/bowyer/testEverything.py
@@ -4,18 +4,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from random import random
+import time
 xsys=[(3,3),(4,3),(5,2),(4,1),(3,1),(2,2)] #hex
 # xsys=[(1,1),(0,2),(2,2),(1,3)] #square
 #xsys=[(1,1),(0,2),(2,3),(1,3),(2,1)] #five
 
 xsys=[]
-for itera in range(0,1000):
+for itera in range(0,2000):
     point=(random()*100,random()*100)
     xsys.append(point)
 
-
+startTime=time.time()
 comp=Delaunay(xsys)
 transformed,normalPoints=comp.computeVertices()
+endTime=time.time()
+print('total',endTime-startTime)
 comp.plotSelf()
 xs=[point[0] for point in xsys]
 ys=[point[1] for point in xsys]
